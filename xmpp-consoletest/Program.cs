91b7dc05f891328cf1e4884f2403ac9830ecac40
Paulius <pauliusval99@hotmail.com>
2018-04-22 13:10:20 +0300

    Add project files.

---
+++ xmpp-consoletest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmpp_consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
